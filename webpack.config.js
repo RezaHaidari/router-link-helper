44ce4f996dbcb9fad89f355494d03c7450c146ee
Reza Haidari <reza@inb-co.com>
2017-03-29 13:55:48 +0430

    rebuild and minified

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
 const path = require('path')
 const production = (process.env.NODE_ENV === 'production')
+process.noDeprecation = true
 
 module.exports = {
     entry:'./src/install.js',
